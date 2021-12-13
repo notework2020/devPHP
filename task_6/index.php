2299bc900dbbde0b4581c75ecdac5dd74a1266aa
Rem Remov <notework2020@yandex.ru>
2021-12-13 17:02:02 +0300

    цикл foreach 6les

---
+++ task_6/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>
+            Подготовительные задания к курсу
+        </title>
+        <meta name="description" content="Chartist.html">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+    </head>
+    <body class="mod-bg-1 mod-nav-link ">
+        <main id="js-page-content" role="main" class="page-content">
+            <div class="col-md-6">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Задание
+                        </h2>
+                        <div class="panel-toolbar">
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        </div>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                           <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+						   
+<?php
+
+$workers = [
+
+[
+    'photo' => 'img/demo/authors/sunny.png',
+    'name' => 'Sunny A.',
+	'nameSpec' => 'Sunny A. (UI/UX Expert)',
+	'prof' => 'Lead Author',
+	'twitter' => 'https://twitter.com/@myplaneticket',
+	'twitName' => '@myplaneticket',
+	'wrapbootstrap' => 'https://wrapbootstrap.com/user/myorange',
+	'title' => 'Contact Sunny',
+	'ban' => false
+	
+],
+
+[
+    'photo' => 'img/demo/authors/josh.png',
+    'name' => 'Jos K.',
+	'nameSpec' => 'Jos K. (ASP.NET Developer)',
+	'prof' => 'Partner &amp; Contributor',
+	'twitter' => 'https://twitter.com/@atlantez',
+	'twitName' => '@atlantez',
+	'wrapbootstrap' => 'https://wrapbootstrap.com/user/Walapa',
+	'title' => 'Contact Jos',
+	'ban' => false
+	
+],
+
+[
+    'photo' => 'img/demo/authors/jovanni.png',
+    'name' => 'Jovanni Lo',
+	'nameSpec' => 'Jovanni L. (PHP Developer)',
+	'prof' => 'Partner &amp; Contributor',
+	'twitter' => 'https://twitter.com/@lodev09',
+	'twitName' => '@lodev09',
+	'wrapbootstrap' => 'https://wrapbootstrap.com/user/lodev09',
+	'title' => 'Contact Jovanni',
+	'ban' => false
+	
+],
+
+[
+    'photo' => 'img/demo/authors/roberto.png',
+    'name' => 'Jovanni Lo',
+	'nameSpec' => 'Roberto R. (Rails Developer)',
+	'prof' => 'Partner &amp; Contributor',
+	'twitter' => 'https://twitter.com/@sildur',
+	'twitName' => '@sildur',
+	'wrapbootstrap' => 'https://wrapbootstrap.com/user/sildur',
+	'title' => 'Contact Roberto',
+	'ban' => true
+	
+]
+
+
+
+];
+
+?>
+						   
+						   
+						   
+						   
+						   
+						   
+						   <?php foreach($workers as $worker):?>
+							<?php if($worker['ban']): ?>
+ 							<div title="Заблокирован" class="banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">								
+                        	 <?php else: ?>
+						   <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                        	 <?php endif;?>
+
+
+                            <img src="<?php echo $worker['photo'] ?>" alt="<?php echo $worker['name'] ?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                              
+
+
+							  <div class="ml-2 mr-3">
+                                    <h5 class="m-0">
+									
+                                        <?php echo $worker['nameSpec'] ?>
+                                        <small class="m-0 fw-300">
+                                            <?php echo $worker['prof'] ?>
+                                        </small>
+                                    </h5>
+
+                                    <?php if($worker['ban']): ?>
+                                    <a><?php echo $worker['twitName'] ?></a> -
+                                    <a><i class="fal fa-envelope"></i></a>
+
+                                    <?php else: ?>
+
+
+                                    <a href="<?php echo $worker['twitter'] ?>" class="text-info fs-sm" target="_blank"><?php echo $worker['twitName'] ?></a> -
+                                    <a href="<?php echo $worker['wrapbootstrap'] ?>" class="text-info fs-sm" target="_blank" title="<?php echo $worker['title'] ?>"><i class="fal fa-envelope"></i></a>
+                                    <?php endif;?>
+
+                                </div>
+                            </div>
+							<?php endforeach; ?>
+							
+							
+							
+                            
+							
+							
+							
+							
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+
+        <script src="js/vendors.bundle.js"></script>
+        <script src="js/app.bundle.js"></script>
+        <script>
+            // default list filter
+            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+            // custom response message
+            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+        </script>
+    </body>
+</html>
